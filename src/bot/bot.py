5306291000fb75509a7e25d3ede1ac280eda6b1a
D4nteDt <ddzovanna3@gmail.com>
2026-04-06 21:33:53 +0500

    add bot

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,18 +1,12 @@
 import asyncio
-
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+from src.bot.handlers import router
 from src.config.settings import BOT_TOKEN
-
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
-@dp.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.answer("Привет!")
-
 async def main():
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
