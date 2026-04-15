f2b805d58a91bf31a66971801e061c1780d6fcd8
D4nteDt <ddzovanna3@gmail.com>
2026-04-15 16:39:38 +0500

    update structure Kalman filter

--- src/analytics/filters.py
+++ src/analytics/filters.py
@@ -2,6 +2,7 @@ from numpy import median, array
 from filterpy.kalman import KalmanFilter
 from filterpy.common import Q_discrete_white_noise
 import matplotlib.pyplot as plt
+import json
 
 def calculate_local_stats(data_window:list):
     local_median = float(median(data_window))
@@ -23,23 +24,60 @@ def hampel_filter(data: list, window_size: int = 7) -> list:
             filtered_data[i] = local_median
     return filtered_data
 
-def kalman_filter(prices: list) -> list:
+class Kalman_filter:
+    def __init__(self, R: float = 0.1, Q: float = 1.0, P: float = 1000.0):
+        self._kf = KalmanFilter(2, 1)
+        self._kf.F = array([[1., 1.], [0., 1.]])
+        self._kf.H = array([[1., 0.]])
+        self._kf.R = array([[R]])
+        self._kf.Q = Q_discrete_white_noise(dim=2, dt=1., var=Q)
+        self._kf.P = P
+    
+    def initialize_state(self, initial_price: float) -> dict:
+        self._kf.x = array([[initial_price], [0.]])
+        self._kf.P = self._kf.P * self.P
+
+    def set_state_from_json(self, x_json: str, P_json: str):
+        self._kf.x = array(json.loads(x_json))
+        self._kf.P = array(json.loads(P_json))
+
+    def get_state_as_json(self) -> tuple[str, str]:
+        return json.dumps(self._kf.x.tolist()), json.dumps(self._kf.P.tolist())
+    
+    def update(self, measurement: float) -> float:
+        self._kf.predict()
+        self._kf.update(measurement)
+        return float(self._kf.x[0, 0])
+
+
+def initialize_kalman_filter(initial_price: float) -> dict:
     kf = KalmanFilter(2, 1)
-    kf.x = array([[prices[0]], [0.]])
+    kf.x = array([[initial_price], [0.]])
     kf.F = array([[1., 1.], [0., 1.]])
     kf.H = array([[1., 0.]])
     kf.R = array([[0.1]])
     kf.Q = Q_discrete_white_noise(dim=2, dt=1., var=1.0)
     kf.P *= 1000.
-    filtered_prices = []
-    for i in prices:
-        kf.predict()
-        kf.update(i)
-        filtered_prices.append(kf.x[0, 0])
-    return filtered_prices
+    return {
+        'x': kf.x.tolist(),
+        'P': kf.P.tolist()
+    }
 
-def system_of_filters(prices: list) -> list:
-    return kalman_filter(hampel_filter(prices))
+def kalman_filter_step(previous_state: dict, current_measurement: float) -> tuple[float, dict]:
+    kf = KalmanFilter(2,1)
+    kf.x = array(previous_state['x'])
+    kf.P = array(previous_state['P'])
+    kf.F = array([[1., 1.], [0., 1.]])
+    kf.H = array([[1., 0.]])
+    kf.R = array([[0.1]])
+    kf.Q = Q_discrete_white_noise(dim=2, dt=1., var=1.0)
+    kf.predict()
+    kf.update(current_measurement)
+    return float(kf.x[0,0]), {
+        'x': kf.x.tolist(),
+        'P': kf.P.tolist()
+    }
+
 
 def plot_results(original, filtered):
     plt.figure(figsize=(12, 6))
